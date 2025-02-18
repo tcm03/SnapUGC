544681e5a293392bd9a91ad3ef60ea57eb2ad2e5
tcm03 <21125019@student.hcmus.edu.vn>
2025-02-18 10:47:27 +0700

    Update organize script with min,max duration and max samples

--- scripts/organize.py
+++ scripts/organize.py
@@ -1,124 +1,6 @@
-import os
-import csv
 import argparse
-import json
-
-def is_float(s):
-    try:
-        float(s)
-        return True
-    except ValueError:
-        return False
-
-def get_label(
-    nawp: int,
-    quartile1: int,
-    quartile3: int
-) -> str:
-    if nawp <= quartile1:
-        return "0"
-    if nawp >= quartile3:
-        return "2"
-    return "1"
-
-def get_input_prompt() -> str:
-    return "<image>\nThis video is a SnapChat video on one of many categories. The engagement rate defined for each such video is based on the average watch time and how long viewers stay engaged before skipping. The higher the average watch time and lower the skip rate, the more engaged the video is. The final prediction label is either 0 (not engaged), 1 (neutral), or 2 (engaged). Please predict one of the three labels for this video, based on its contents only."
-
-def get_output_response(label: str) -> str:
-    return f"The engagement label of the video is {label}."
-
-def prepare_train_json(
-    raw_train: str,
-    train_dir: str,
-    train_json_path: str,
-    quartile1: int,
-    quartile3: int
-):
-    print(f"Preparing training json file...")
-    train_json = []
-    with open(raw_train, "r", encoding="utf-8") as file:
-        reader = csv.reader(file, delimiter="\t")
-        next(reader)  # Skip header row
-        train_dir_files = os.listdir(train_dir)
-        num_samples = 0
-        for row in reader:
-            if row[1] == '' or row[2] == '' or row[3] == '':
-                print(f"Empty row found: {row[0]}")
-                continue
-            if not is_float(row[1]) or not row[2].isdigit() or not row[3].isdigit():
-                print(f"Non-numeric row found: {row[0]}")
-                continue
-            vid = str(row[0])
-            if f"{vid}.mp4" in train_dir_files:
-                nawp = int(row[3])
-                label = get_label(nawp, quartile1, quartile3)
-                train_json.append({
-                    "video": f"train/{vid}.mp4",
-                    "label": label,
-                    "conversations": [
-                        {
-                            "from": "human",
-                            "value": get_input_prompt()
-                        },
-                        {
-                            "from": "gpt",
-                            "value": get_output_response(label)
-                        }
-                    ]
-                })
-                num_samples += 1
-        print(f"Number of training samples prepared: {num_samples}")
-            
-    with open(train_json_path, "w", encoding="utf-8") as file:
-        json.dump(train_json, file, indent=4)
-
-
-def prepare_test_json(
-    raw_test: str,
-    test_dir: str,
-    test_json_path: str,
-    quartile1: int,
-    quartile3: int
-):
-    print(f"Preparing testing json file...")
-    test_json = []
-    with open(raw_test, "r", encoding="utf-8") as file:
-        reader = csv.reader(file, delimiter="\t")
-        next(reader)  # Skip header row
-        test_dir_files = os.listdir(test_dir)
-        num_samples = 0
-        for row in reader:
-            if row[1] == '' or row[2] == '' or row[3] == '':
-                print(f"Empty row found: {row[0]}")
-                continue
-            if not is_float(row[1]) or not row[2].isdigit() or not row[3].isdigit():
-                print(f"Non-numeric row found: {row[0]}")
-                continue
-            vid = str(row[0])
-            if f"{vid}.mp4" in test_dir_files:
-                nawp = int(row[3])
-                label = get_label(nawp, quartile1, quartile3)
-                test_json.append({
-                    "video": f"test/{vid}.mp4",
-                    "label": label,
-                    "conversations": [
-                        {
-                            "from": "human",
-                            "value": get_input_prompt()
-                        },
-                        {
-                            "from": "gpt",
-                            "value": get_output_response(label)
-                        }
-                    ]
-                })
-                num_samples += 1
-        print(f"Number of testing samples prepared: {num_samples}")
-            
-    with open(test_json_path, "w", encoding="utf-8") as file:
-        json.dump(test_json, file, indent=4)
-
-
+from constants import *
+from utils import prepare_json_data
 
 if __name__ == "__main__":
 
@@ -132,10 +14,34 @@ if __name__ == "__main__":
     parser.add_argument('--test_json', type=str, required=True, help="Path to the testing json file")
     parser.add_argument('--train_dir', type=str, required=False, default="train", help="Path to the train directory (of clips)")
     parser.add_argument('--test_dir', type=str, required=False, default="test", help="Path to the test directory (of clips)")
-    parser.add_argument('--quartile1', type=int, required=False, default=32769, help="First quartile for labeling")
-    parser.add_argument('--quartile3', type=int, required=False, default=98323, help="Third quartile for labeling")
+    parser.add_argument('--min_duration', type=int, required=False, default=DEFAULT_MIN_DURATION, help="Minimum duration of clips to consider")
+    parser.add_argument('--max_duration', type=int, required=False, default=DEFAULT_MAX_DURATION, help="Maximum duration of clips to consider")
+    parser.add_argument('--max_samples_train', type=int, required=False, default=None, help="Maximum number of training samples to consider")
+    parser.add_argument('--max_samples_test', type=int, required=False, default=None, help="Maximum number of testing samples to consider")
+    parser.add_argument('--quartile1', type=int, required=False, default=DEFAULT_QUARTILE1, help="First quartile for labeling")
+    parser.add_argument('--quartile3', type=int, required=False, default=DEFAULT_QUARTILE3, help="Third quartile for labeling")
     # parser.add_argument('--num_workers', type=int, required=False, default=16, help="Number of workers to use for downloading")
     
     args = parser.parse_args()  # Parse the arguments
-    prepare_train_json(args.raw_train, args.train_dir, args.train_json, args.quartile1, args.quartile3)
-    prepare_test_json(args.raw_test, args.test_dir, args.test_json, args.quartile1, args.quartile3)
+    prepare_json_data(
+        raw_file = args.raw_train, 
+        data_dir = args.train_dir, 
+        output_json_path = args.train_json, 
+        quartile1 = args.quartile1, 
+        quartile3 = args.quartile3, 
+        prefix="train",
+        min_duration = args.min_duration,
+        max_duration = args.max_duration,
+        max_samples = args.max_samples_train
+    )
+    prepare_json_data(
+        raw_file = args.raw_test, 
+        data_dir = args.test_dir, 
+        output_json_path = args.test_json, 
+        quartile1 = args.quartile1, 
+        quartile3 = args.quartile3, 
+        prefix="test",
+        min_duration = args.min_duration,
+        max_duration = args.max_duration,
+        max_samples = args.max_samples_test
+    )
---
+++ scripts/utils.py
@@ -0,0 +1,86 @@
+import os
+import csv
+import argparse
+import json
+from constants import *
+import random
+
+def get_input_prompt() -> str:
+    return "<image>\nThis video is a SnapChat video on one of many categories. The engagement rate defined for each such video is based on the average watch time and how long viewers stay engaged before skipping. The higher the average watch time and lower the skip rate, the more engaged the video is. The final prediction label is either 0 (not engaged), 1 (neutral), or 2 (engaged). Please predict one of the three labels for this video, based on its contents only."
+
+def get_output_response(label: str) -> str:
+    return f"The engagement label of the video is {label}."
+
+def is_float(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+def get_label(
+    nawp: int,
+    quartile1: int,
+    quartile3: int
+) -> str:
+    if nawp <= quartile1:
+        return "0"
+    if nawp >= quartile3:
+        return "2"
+    return "1"
+
+def prepare_json_data(
+    raw_file: str,
+    data_dir: str,
+    output_json_path: str,
+    **kwargs
+):
+    print(f"Preparing {prefix} json file...")
+    data_json = []
+    quartile1 = kwargs.get("quartile1", DEFAULT_QUARTILE1)
+    quartile3 = kwargs.get("quartile3", DEFAULT_QUARTILE3)
+    prefix = kwargs.get("prefix", "data")
+    min_duration = kwargs.get("min_duration", DEFAULT_MIN_DURATION)
+    max_duration = kwargs.get("max_duration", DEFAULT_MAX_DURATION)
+    with open(raw_file, "r", encoding="utf-8") as file:
+        reader = csv.reader(file, delimiter="\t")
+        next(reader)  # Skip header row
+        data_files = os.listdir(data_dir)
+        num_samples = 0
+        for row in reader:
+            if row[1] == '' or row[2] == '' or row[3] == '':
+                print(f"Empty row found: {row[0]}")
+                continue
+            if not is_float(row[1]) or not row[2].isdigit() or not row[3].isdigit():
+                print(f"Non-numeric row found: {row[0]}")
+                continue
+            vid = str(row[0])
+            duration = float(row[1])
+            if f"{vid}.mp4" in data_files and min_duration <= int(duration) <= max_duration:
+                nawp = int(row[3])
+                label = get_label(nawp, quartile1, quartile3)
+                full_path = f"{prefix}/{vid}.mp4"
+                data_json.append({
+                    "video": full_path,
+                    "label": label,
+                    "conversations": [
+                        {
+                            "from": "human",
+                            "value": get_input_prompt()
+                        },
+                        {
+                            "from": "gpt",
+                            "value": get_output_response(label)
+                        }
+                    ]
+                })
+                num_samples += 1
+        print(f"Number of {prefix} samples prepared: {num_samples}")
+
+    max_samples = kwargs.get("max_samples", None)
+    sample_data_json = data_json
+    if max_samples is not None:
+        sample_data_json = random.sample(data_json, min(max_samples, len(sample_data_json)))
+
+    with open(output_json_path, "w", encoding="utf-8") as file:
+        json.dump(sample_data_json, file, indent=4)
